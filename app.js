febd433e749f15a586a26c3b1a0c70014c19c463
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-04-23 14:14:04 -0700

    create book constructor. hide create book window.

---
+++ app.js
@@ -0,0 +1,26 @@
+const bookName = document.getElementById('name');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const bookRead = document.getElementById('read');
+const submit = document.getElementById('submit');
+
+bookList = [];
+
+function Book(name, author, pages, read) {
+	this.name = name;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+submit.addEventListener('click', (e) => {
+	e.preventDefault();
+	book = new Book(
+		bookName.value,
+		bookAuthor.value,
+		bookPages.value,
+		bookRead.value
+	);
+	bookList.push(book);
+	console.log('clicked');
+});
