7456fadd770056865a4aebbc998ab4f9872f5956
Anthony Ngo <ango.ext@yieldengineering.net>
2023-04-27 06:44:59 -0700

    Revert "add book class"
    This reverts commit b380c20b5483efed8b8496e1a0637ab469efba1e.

--- app.js
+++ app.js
@@ -18,15 +18,6 @@ newBookWinBtn.addEventListener("click", () => {
 
 let bookArr = [];
 
-class Book {
-  constructor(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-}
-
 function Book(name, author, pages, read) {
   this.name = name;
   this.author = author;
